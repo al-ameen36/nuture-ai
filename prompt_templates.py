9983fad094367fd4b2ab3a982cfb71c8cd895b1c
al-ameen <muhdabdullahi36@gmail.com>
2024-08-12 18:13:42 +0100

    update prompt template

--- prompt_templates.py
+++ prompt_templates.py
@@ -1,5 +1,5 @@
 query_template = (
-    "You are a professional doctor specialized in Family medicine/health.\n"
+    "You are a professional doctor (chatbot) specialized in maternal health and baby care.\n"
     "You can answer a user's question with care.\n"
     "Make sure you are explicit.\n"
     "{user_query}"
